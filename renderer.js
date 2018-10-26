09e2e737e1751772cf5f47be59b415dd236d48bd
fohristiwhirl <fohristiwhirl@users.noreply.github.com>
2018-10-26 16:28:27 +0100

    Update renderer.js

--- renderer.js
+++ renderer.js
@@ -20,7 +20,7 @@ function make_token_parser() {
 
 	let o = Object.create(null);
 
-	let tokens = [];		// private
+	let tokens = [];		// Private
 
 	let scanner = readline.createInterface({
 		input: process.stdin,
@@ -37,20 +37,17 @@ function make_token_parser() {
 	};
 
 	o.token = () => {
-		if (tokens.length === 0) {
-			return null;
-		}
 		return tokens.shift();
 	};
 
 	o.int = () => {
-		if (tokens.length === 0) {
-			return null;
-		}
-		let raw = tokens.shift();
-		let val = parseInt(raw, 10);
-		return val;
+		return parseInt(tokens.shift(), 10);
 	};
+
+	o.peek_int = (n) => {
+		return parseInt(tokens[n], 10);
+	};
+
 
 	return o;
 }
@@ -77,15 +74,28 @@ function make_game() {
 
 	let game = Object.create(null);
 
+	game.clean = false;		// Can be rendered?
+
 	game.players = null;
+	game.pid = null;
 	game.width = null;
 	game.height = null;
-	game.pid = null;
+	game.turn = null;
 
 	game.budgets = Object.create(null);
 	game.ships = [];
 	game.dropoffs = [];
 	game.halite = null;
+
+	game.init_map = () => {
+		game.halite = [];
+		for (let x = 0; x < game.width; x++) {
+			game.halite.push([]);
+			for (let y = 0; y < game.height; y++) {
+				game.halite[x].push(0);
+			}
+		}
+	}
 
 	return game;
 }
@@ -112,12 +122,12 @@ function make_renderer() {
 		tp.token()
 
 		renderer.pre_parse();
-	}
+	};
 
 	renderer.pre_parse = () => {
 
 		if (tp.count() < 2) {
-			setTimeout(pre_parse, 1);
+			setTimeout(renderer.pre_parse, 1);
 			return;
 		}
 
@@ -125,14 +135,14 @@ function make_renderer() {
 		game.pid = tp.int();
 
 		renderer.parse_factories();
-	}
+	};
 
 	renderer.parse_factories = () => {
 
 		let tokens_needed = game.players * 3;
 
 		if (tp.count() < tokens_needed) {
-			setTimeout(parse_factories, 1);
+			setTimeout(renderer.parse_factories, 1);
 			return;
 		}
 
@@ -144,11 +154,145 @@ function make_renderer() {
 			game.dropoffs.push(factory);
 		}
 
+		renderer.parse_width_height();
+	};
 
+	renderer.parse_width_height = () => {
 
+		if (tp.count() < 2) {
+			setTimeout(renderer.parse_width_height, 1);
+			return;
+		}
 
+		game.width = tp.int();
+		game.height = tp.int();
 
+		renderer.parse_map();
+	};
 
+	renderer.parse_map = () => {
+
+		let tokens_needed = width * height;
+
+		if (tp.count() < tokens_needed) {
+			setTimeout(renderer.parse_map, 1);
+			return;
+		}
+
+		game.init_map();
+
+		for (let y = 0; y < game.height; y++) {
+			for (let x = 0; x < game.width; x++) {
+				game.halite[x][y] = tp.int();
+			}
+		}
+
+		setTimeout(renderer.loop, 0);		// Eh, it's nice to clear the stack.
+	};
+
+	// --------------------------------------------------------------
+
+	renderer.loop = () => {
+
+		// bare_min_tokens_needed is the absolute bare minimum number
+		// of tokens that might be capable of forming the frame, given
+		// what we actually know. It is updated as we gain more info.
+
+		let bare_min_tokens_needed = 1 + (game.players * 4) + 1;
+
+		if (tp.count() < bare_min_tokens_needed) {
+			setTimeout(renderer.loop, 1);
+			return;
+		}
+
+		// --------------------
+
+		let info_index = 1;
+
+		for (let z = 0; z < game.players; z++) {
+
+			let pid = tp.peek_int(info_index + 0);
+			let ships = tp.peek_int(info_index + 1);
+			let dropoffs = tp.peek_int(info_index + 2);
+
+			// Update our bare minimums and check...
+
+			bare_min_tokens_needed += ships * 4;
+			bare_min_tokens_needed += dropoffs * 3;
+
+			if (tp.count() < bare_min_tokens_needed) {
+				setTimeout(renderer.loop, 1);
+				return;
+			}
+
+			info_index += 4 + (ships * 4) + (dropoffs * 3);
+
+		}
+
+		let map_updates = tp.peek_int(info_index);
+
+		bare_min_tokens_needed += map_updates * 3;
+
+		if (tp.count() < bare_min_tokens_needed) {
+			setTimeout(renderer.loop, 1);
+			return;
+		}
+
+		// --------------------
+		// The tokens exist!
+
+		game.ships = [];
+
+		// Clear the dropoffs but save the factories...
+		game.dropoffs = game.dropoffs.slice(0, game.players);
+
+		game.turn = tp.int();
+
+		for (let n = 0; n < game.players; n++) {
+
+			let pid = tp.int();
+			let ships = tp.int();
+			let dropoffs = tp.int();
+
+			game.budgets[pid] = tp.int();
+
+			for (let i = 0; i < ships; i++) {
+
+				let sid = tp.int();
+				let x = tp.int();
+				let y = tp.int();
+				let halite = tp.int();
+
+				game.ships.push(make_ship(pid, sid, x, y, halite));
+			}
+
+			for (let i = 0; i < dropoffs; i++) {
+
+				tp.int();			// sid
+				let x = tp.int();
+				let y = tp.int();
+
+				game.dropoffs.push(make_dropoff(pid, x, y));
+			}
+		}
+
+		map_updates = tp.int();
+
+		for (let n = 0; n < map_updates; n++) {
+
+			let x = tp.int();
+			let y = tp.int();
+			let val = tp.int();
+
+			game.halite[x][y] = val;
+		}
+
+		console.log(`Finished reading turn ${game.turn}`);
+
+		game.clean = true;
+
+		// FIXME: setTimeout for self
+	};
 
 	// --------------------------------------------------------------
 
